10ac667f332c119bfd24ee4f36f333b6a85635d6
EliasSherem <sheremelias@gmail.com>
2025-05-02 12:03:35 -0600

    Nuevo CLI con apuntes de clase

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,53 @@
-import { readFileSync } from 'node:fs';
-import {parseArgs} from 'node:util';
+import { parseArgs } from "node:util";
+import { readTasks, writeTasks } from "./file-ops";
+import { Task } from "./types";
 
+/**
+ * Options accepted from the command line.
+ */
 const parsingOptions = {
-    name: {type: "string", required: true} as const,
-    description: {type: "string"} as const,
+  // Action to take
+  create: { type: "boolean" } as const,
+  read: { type: "boolean" } as const,
+  name: { type: "string" } as const,
+  description: { type: "string" } as const,
 };
-function main(){
-    const args = parseArgs({options: parsingOptions});
-    const data = readFileSync("C:/Users/shere/OneDrive/EBIS/Backend/tasks.json", "utf-8")
-    console.log(data);
+
+function main () {
+  const args = getArgs();
+
+  if (args.read) {
+    console.log(readTasks());
+  } else if (args.create && args.name) {
+    createTask(args.name, args.description);
+  } else {
+    console.log("Nothing to do:\n", args);
+  }
 }
+
+function getArgs() {
+  try {
+    const { values } = parseArgs({ options: parsingOptions});
+
+    return values;
+  } catch (error) {
+    const typedError = error as Error;
+    console.error(`${typedError.name}"\n${typedError.message}`);
+
+    process.exit(1);
+  }
+}
+
+function createTask(name: string, description?: string|null): void {
+  const newTask = {name, description} as Task;
+
+  const tasks = readTasks();
+
+  tasks.push(newTask);
+
+  writeTasks(tasks);
+
+  console.log(`New task added: \n\n${name}\n${description}`);
+}
+
 main();
